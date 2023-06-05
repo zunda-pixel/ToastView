b3eb1bf045a6ef34a9d996436b25da0cc66174fe
zunda <zunda.dev@gmail.com>
2023-06-05 14:41:37 +0900

    add ToastView

--- Sources/ToastView/ToastView.swift
+++ Sources/ToastView/ToastView.swift
@@ -1,6 +1,42 @@
-public struct ToastView {
-    public private(set) var text = "Hello, World!"
+//
+//  ToastView.swift
+//
 
-    public init() {
+import SwiftUI
+
+extension View {
+  func toastView() -> some View {
+    background(Color(uiColor: .systemBackground))
+    .clipShape(Capsule())
+    .shadow(
+      color: Color.secondary.opacity(0.5),
+      radius: 10
+    )
+    .transition(.move(edge: .top).combined(with: .opacity))
+  }
+}
+
+struct ToastView_Preview: PreviewProvider {
+  struct ApplePencilView: View {
+    var body: some View {
+      VStack(spacing: 4) {
+        Text("Apple Pencil")
+          .bold()
+        
+        Text("\(Text("40%").foregroundColor(.secondary)) \(Image(systemName: "battery.50"))").foregroundColor(.green)
+      }
     }
+  }
+  
+  static var previews: some View {
+    ApplePencilView()
+      .frame(maxWidth: 200, maxHeight: 60)
+      .toastView()
+      .preferredColorScheme(.light)
+      
+    ApplePencilView()
+      .frame(maxWidth: 200, maxHeight: 60)
+      .toastView()
+      .preferredColorScheme(.dark)
+  }
 }
